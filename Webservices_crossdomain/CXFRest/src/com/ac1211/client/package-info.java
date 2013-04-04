a590a5922be4978d6f79f3ae7963ccc06343cb14
balamuralikrishnan <muralikrishnan.a@tcs.com>
2013-04-04 20:28:00 +0530

    Removed unWanted Project

--- Webservices_crossdomain/CXFRest/src/com/ac1211/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.moneygram.com/AgentConnect1211", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.ac1211.client;
