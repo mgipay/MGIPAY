1527b313e1ade8324b3aa395db0ba079c6ee89c7
Monica <monica1@static-224.115.96.14-tataidc.co.in>
2013-04-24 20:22:08 +0530

    Mobile and desktop HTML Pages

---
+++ MOBILE HTML/MG_Mobile_Ver0.1/scripts/Nonauthenticatedwithdraw.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	
+				$("#fee_withdrawCash").click(function() {
+					callAddCashResponse();
+					$('#fee_tooltip_withdrawCash').slideDown(800);
+				});
+				$("#fancybox-close_withdrawCash").click(function() {
+					$('#fee_tooltip_withdrawCash').hide(800);
+				});
+				
+				$("#fee_limitFee").click(function() {
+					callFeeLimitResponse();
+					$('#fee_tooltip_limitFee').slideDown(800);
+					//$('#fee_tooltip').hide(800);
+				});
+				
+				$("#fancybox-close_limitFee").click(function() {
+					$('#fee_tooltip_limitFee').hide(800);
+				});
+				
+				
+});
