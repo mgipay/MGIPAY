2ba3873debd4eefee7590b5fc047bb80da8911ad
Monica <monica1@static-109.205.99.14-tataidc.co.in>
2013-05-06 17:19:35 +0530

    Final UAT code

---
+++ MGIPay HTML/UAT_MGIPAY/UAT_FINALVersion/scripts/back.js
@@ -0,0 +1,27 @@
+function noBack(){
+	window.history.forward();
+	
+  setTimeout(function(){
+
+  noBack();
+},0);
+	}
+	var logoutbutton;
+$(document).ready(function() {
+	$.browser.chrome = /chrome/.test(navigator.userAgent.toLowerCase());
+	if($.browser.chrome)
+	{
+		logoutbutton = localStorage.logoutClicked;
+	}
+	else 
+	{
+		logoutbutton = $.cookie("logoutClicked");
+	}
+	if(logoutbutton == "true"){
+		$("#preview").hide();
+		noBack();
+		window.onload=noBack;
+		window.onpageshow=function(evt){if(evt.persisted)noBack()}
+		window.onunload=function(){void(0)}
+	}
+});
