6cd32bbc22ce296851162f2f335f50323af4260e
Monica <monica1@static-56.1.96.14-tataidc.co.in>
2013-05-07 20:21:41 +0530

    Mobile Code with add cash url updated

--- MOBILE HTML/MG_Mobile_Ver0.7/scripts/global.js
+++ MOBILE HTML/MG_Mobile_Ver0.7/scripts/global.js
@@ -1,6 +1,6 @@
 /** This file contains global constants like service urls/ service method names */
 //var baseUrl = "https://54.236.98.103/services/CXFRest/rest/"; 
-var baseUrl = "https://devpaypal.qa.moneygram.com/services/CXFRest/rest/";
+var baseUrl = "https://devpaypal.qa.moneygram.com/CXFRest/rest/";
 var methods = {
 	GetFees : ["getFee", "POST"],
 	CodeTable : ["getStateCode", "POST"],
---
+++ MOBILE HTML/MG_Mobile_Ver0.7/scripts/tabContent.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	// Adding year dynamically in footer
+	//$(".footer_year").text( (new Date).getFullYear() );
+	$(".footer_year").text((new Date).getFullYear() + " - " + (parseInt((new Date).getFullYear()) + 1));
+
+	var Active = "no";
+	$("#menuexpandId").click(function() {
+		if (Active == "no") {
+
+			$('#navfootermenu').slideDown();
+			document.getElementById("menuexpandId").style.backgroundImage = "url(images/footer_collapsed_icon.png)";
+			Active = "yes";
+		} else {
+			$('#navfootermenu').slideUp();
+			document.getElementById("menuexpandId").style.backgroundImage = "url(images/footer_expand_icon.png)";
+			Active = "no";
+		}
+
+	});
+	var menu3 = $('#menuexpandId');
+	$(document).click(function(e) {
+
+		if (e.target != menu3.get(0) && menu3.find($(e.target)).length == 0) {
+
+			$('#navfootermenu').hide();
+		
+			document.getElementById("menuexpandId").style.backgroundImage = "url(images/footer_expand_icon.png)";
+			Active = "no";
+
+		}
+
+	});
+
+});
+
+function goTowithdrawPage() {
+	window.location.href = "withdrawcash.html";
+}
+
+function openHistoryDetails() {
+	window.location.href = "history.html";
+}
