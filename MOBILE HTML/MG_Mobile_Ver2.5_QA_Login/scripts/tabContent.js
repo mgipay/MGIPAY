9a275f4b2ec93fd8ece31754cd9b2e7de0e4fb9d
Monica <monica1@static-155.83.99.14-tataidc.co.in>
2013-06-03 19:17:20 +0530

    Latest Mobile code

---
+++ MOBILE HTML/MG_Mobile_Ver2.5_QA_Login/scripts/tabContent.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	// Adding year dynamically in footer
+	$(".footer_year").text((new Date).getFullYear());
+	var Active = "no";
+	$("#menuexpandId").click(function() {
+		if (Active == "no") {
+			$('#navfootermenu').slideDown();
+			document.getElementById("menuexpandId").style.backgroundImage = "url(images/footer_collapsed_icon.png)";
+			Active = "yes";
+		} else {
+			$('#navfootermenu').slideUp();
+			document.getElementById("menuexpandId").style.backgroundImage = "url(images/footer_expand_icon.png)";
+			Active = "no";
+		}
+	});
+	
+	/*** script for closing of the bottom menu, where ever click in the document ***/
+	var menu3 = $('#menuexpandId');
+		$(document).click(function(e) {
+		if (e.target != menu3.get(0) && menu3.find($(e.target)).length == 0) {
+			$('#navfootermenu').hide();
+			document.getElementById("menuexpandId").style.backgroundImage = "url(images/footer_expand_icon.png)";
+			Active = "no";
+		}
+	});
+
+	/***** script for closing of the bottom menu, when click on any tab ***/
+	$(".sectioncontent").bind("touchstart", function(e){
+			$('#navfootermenu').hide();	
+			document.getElementById("menuexpandId").style.backgroundImage = "url(images/footer_expand_icon.png)";
+			Active = "no";
+	});
+});
---
+++ MOBILE HTML/MG_Mobile_Ver2.5_QA_Login/scripts/utility.js
@@ -0,0 +1,23 @@
+
+var resources = {
+	'country' : 'United States',
+	'availableTime' : '10 Minutes',
+	'errorMsg' : 'An error has occurred while processing. Please try again. If you continue to receive this error, contact MoneyGram at 1-800-469-4993. Thank you.',
+	'validAmt' : 'Please enter valid receive amount',
+	'validLocation' : 'Please Select Pick Up Location',
+	'selectState' : 'Select your state',
+	'user' : 'MGI',
+	'firstNameMsg' : 'Please enter first name properly',
+	'lastNameMsg' : 'Please enter last name properly',
+	'emailMsg' : 'Please enter valid email address',
+	'PhoneNumMsg' : 'Please enter valid Phone number',
+	'msgContactUs' : 'Please enter Message',
+	'UScustomer' : 'This service is only available for PayPal accounts in the US. Thank you for your interest in using MoneyGram and PayPal.',
+	'amtLessthanFee' : 'Amount available to withdraw is less than the receive amount',
+	'sessionError' : 'Your session has expired. Please try again',
+	'refNumber' : 'Please enter Reference Number',
+	'printError' : 'Print functionality is not available in this device',
+	'SMSError' : 'This feature is not available in your device',
+	'transactionDateMsg' : 'Please provide valid transaction Date',
+	'transactionAmtMsg' : 'Please provide valid transaction Date'
+	}
